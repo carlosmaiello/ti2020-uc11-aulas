c64d0c9e72bfd2e44b6f59d420220b45de81baac
Carlos Maiello <carlosmaiello@gmail.com>
2021-06-16 20:19:49 -0300

    Alterando FormCadastro > Cadastro

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'form-cadastro', component: () => import('pages/FormCadastro.vue') },
+      { path: 'form-cadastro', component: () => import('src/pages/Cadastro.vue') },
       { path: 'login', component: () => import('pages/Login.vue') }
     ]
   },
