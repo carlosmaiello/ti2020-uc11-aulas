49e3e0f556276e497ac1b46107657d02952aea12
Carlos Maiello <carlosmaiello@gmail.com>
2021-06-16 19:00:53 -0300

    primeiro commit

---
+++ src/pages/FormCadastro.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page padding>
+    <h3>Cadastro de Cliente</h3>
+    <q-form class="q-gutter-md">
+      <div class="row">
+        <div class="col">
+          <q-input label="Teste" />
+        </div>
+        <div class="col">
+          <q-input label="Teste" />
+        </div>
+      </div>
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-input label="Teste" />
+      <q-checkbox label="Teste" />
+      <div>
+        <q-radio label="Teste" />
+        <q-radio label="Teste" />
+        <q-radio label="Teste" />
+        <q-radio label="Teste" />
+      </div>
+      <div>
+        <q-btn label="Teste" />
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <q-form class="q-gutter-sm">      
+      <q-input outlined label="E-mail" />
+      <q-input outlined label="Senha" />
+      <div>
+        <q-btn color="primary" icon="login" label="Entrar" class="full-width"/>
+      </div>
+    </q-form>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
