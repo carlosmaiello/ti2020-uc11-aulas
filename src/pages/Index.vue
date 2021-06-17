ab6bc8dfc88fae634ad8169a627692285bc36f64
Carlos Maiello <carlosmaiello@gmail.com>
2021-06-16 21:38:55 -0300

    Botão largura total

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
-  <q-page class="flex flex-center column">
+  <q-page class="flex flex-center column items-stretch" padding>
+    <h4>Atividades das Aulas</h4>
       <q-btn label="Cadastro" to="/cadastro" />
-      <q-btn label="Login" to="/login" />  
+      <q-btn label="Login" to="/login"  />
   </q-page>
 </template>
 
